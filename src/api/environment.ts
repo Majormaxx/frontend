895c0a5dfb32ec8fbd9c862fc71f666d3f45ddcc
Emerson majormaxx daniel <emersondaniel300@gmail.com>
2025-09-23 16:55:22 +0100

    feat(config): refactor to use .env for environment variables

--- src/api/environment.ts
+++ src/api/environment.ts
@@ -10,6 +10,10 @@ export const envVariables = {
     apiUrl: "VITE_APP_BASE_URL",
     teamPointsFactoryAddress: "VITE_APP_TEAM_POINTS_FACTORY_ADDRESS",
     teamPointsFactoryAddressCelo: "VITE_APP_TEAM_POINTS_FACTORY_ADDRESS_CELO",
+    rainbowProjectId: "VITE_APP_RAINBOW_PROJECT_ID",
+    walletConnectProjectId: "VITE_APP_WALLETCONNECT_PROJECT_ID",
+    arbitrumSepoliaRpc: "VITE_APP_ARBITRUM_SEPOLIA_RPC",
+    celoAlfajoresRpc: "VITE_APP_CELO_ALFAJORES_RPC",
     appUrl: "VITE_APP_URL",
     network: "VITE_APP_NETWORK",
     blockExplorer: "VITE_APP_BLOCK_EXPLORER"
